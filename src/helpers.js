1637432ed80ed581cb60ecb77bd570643be2e7e5
mariusgessler <marius.gessler@gmail.com>
2020-04-30 20:00:37 +0200

    modified regex

--- src/helpers.js
+++ src/helpers.js
@@ -3,7 +3,7 @@ const getRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const formatActivity = (activity) => activity.trim().charAt(0).toUpperCase() + activity.substring(1).toLowerCase();
 
 const validateActivity = (activity) => {
-  const regex = /^[-0-9a-zA-Z/,\s]{4,35}$/;
+  const regex = /^[-0-9a-zA-Z/,!\s]{4,35}$/;
 
   if (!regex.test(activity)) {
     const msg = 'Please make sure your activity has between 4 and 35 letters and contains no special characters - Thanks!😌';
